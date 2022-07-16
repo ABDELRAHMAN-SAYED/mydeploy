83be0cdf4e18da333ed6abbb42fa932cad472b7f
ABDELRAHMAN-SAYED <66165085+ABDELRAHMAN-SAYED@users.noreply.github.com>
2022-07-16 17:04:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,41 +3,6 @@
 
 # In[8]:
 
-
-from generatcap import *
-import fitz # PyMuPDF
-import io
-from PIL import Image
-def extract_text_caption_image(file_path):
-    words = ""
-    pdf_file = fitz.open(file_path)
-
-    for page_index in range(len(pdf_file)):
-        page = pdf_file[page_index]
-        text = pdf_file[page_index].get_text_words()
-        for i in range(len(text)):
-            words += text[i][4]+" "
-      # get the page itself
-        image_list = page.get_images()
-        # printing number of images found in this page
-        if image_list:
-            print(f"[+] Found a total of {len(image_list)} images in page {page_index}")
-        else:
-            print("[!] No images found on page", page_index)
-        for image_index, img in enumerate(page.get_images(), start=1):
-            # get the XREF of the image
-            xref = img[0]
-            # extract the image bytes
-            base_image = pdf_file.extract_image(xref)
-            image_bytes = base_image["image"]
-            # get the image extension
-            image_ext = base_image["ext"]
-            image = Image.open(io.BytesIO(image_bytes))
-            image = np.array(image)
-            words+="the caption(( "+caption_this_image_for_pdf(image)+")) "
-    return words
-
-
 # In[ ]:
 
 
